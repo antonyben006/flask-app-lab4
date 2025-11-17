772ec57a4ebf32dd25c1911df8cab9bfd1139219
Antony Ben <73778573+antonyben006@users.noreply.github.com>
2025-11-17 18:25:10 -0500

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_external_ip" {
-  value = google_compute_instance.flask_vm.network_interface[0].access_config[0].nat_ip
-}
-
-output "app_url" {
-  value = format("http://%s:5000", google_compute_instance.flask_vm.network_interface[0].access_config[0].nat_ip)
-}
